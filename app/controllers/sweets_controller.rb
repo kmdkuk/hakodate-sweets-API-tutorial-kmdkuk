4158dce17a3b16e0103c910970fdc2538ed0061d
kouki <kouworld0123@gmail.com>
2020-06-17 06:43:57 +0900

    お菓子と店舗を関連付けたsweet APiの作成

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,2 +1,54 @@
 class SweetsController < ApplicationController
+  before_action :set_sweet, only: %i[show update destroy]
+  def index
+    @sweets = nil
+    if params[:shop_id].present?
+      shop = Shop.find(params[:shop_id])
+      @sweets = shop.sweets.all
+    elsif params[:category_id].present?
+      category = Category.find(params[:category_id])
+      @sweets = category.sweets.all
+    else
+      @sweets = Sweet.all
+    end
+    render json: @sweets
+  end
+
+  def show
+    render json: @sweet
+  end
+
+  def create
+    @sweet = Sweet.new(sweet_params)
+    shop = Shop.find(params[:shop_id])
+    @sweet.shop = shop
+
+    if @sweet.save
+      render json: @sweet, status: :created, location: @sweet
+    else
+      render json: @sweet.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @sweet.update(sweet_params)
+      render json: @sweet
+    else
+      render json: @sweet.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @sweet.destroy
+  end
+
+  private
+
+  def set_sweet
+    @sweet = Sweet.find(params[:id])
+  end
+
+  def sweet_params
+    params.require(:sweet).permit(:name, :description, :price, :imagePath)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :shops
-  resources :categories
+  resources :shops do
+    resources :sweets, only: %i[index create]
+  end
+  resources :categories do
+    resources :sweets, only: :index
+  end
+
+  resources :sweets, except: :create
 end
