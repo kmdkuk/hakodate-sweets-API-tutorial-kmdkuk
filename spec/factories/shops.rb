f6af101bbb97e05514c0bbbc43fad51036621cc2
kouki <kouworld0123@gmail.com>
2020-06-17 04:38:30 +0900

    docker-compose run --rm app bundle exec rails g model Shop name:string description:string area:string postcode:string address:string openingHoursSpecification:string closed:string parking:string eatin:string email:string latitude:string longitude:string telephone:string imagePath:string

---
+++ spec/factories/shops.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :shop do
+    name { "MyString" }
+    description { "MyString" }
+    area { "MyString" }
+    postcode { "MyString" }
+    address { "MyString" }
+    openingHoursSpecification { "MyString" }
+    closed { "MyString" }
+    parking { "MyString" }
+    eatin { "MyString" }
+    email { "MyString" }
+    latitude { "MyString" }
+    longitude { "MyString" }
+    telephone { "MyString" }
+    imagePath { "MyString" }
+  end
+end
